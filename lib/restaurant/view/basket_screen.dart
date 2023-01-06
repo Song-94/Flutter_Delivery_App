2b01e7d1f12e94d85a16ade396ed749a388944df
Song-94 <alsdn726@gmail.com>
2023-01-06 21:41:04 +0900

    add order screen

--- lib/restaurant/view/basket_screen.dart
+++ lib/restaurant/view/basket_screen.dart
@@ -1,21 +1,153 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_delivery_app/common/const/colors.dart';
 import 'package:flutter_delivery_app/common/layout/default_layout.dart';
+import 'package:flutter_delivery_app/order/provider/order_provider.dart';
+import 'package:flutter_delivery_app/order/view/order_done_screen.dart';
+import 'package:flutter_delivery_app/product/component/product_card.dart';
+import 'package:flutter_delivery_app/user/provider/basket_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
-class BasketScreen extends ConsumerWidget {
+class BasketScreen extends ConsumerStatefulWidget {
   static String get routeName => 'basket';
 
   const BasketScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(
-    BuildContext context,
-    WidgetRef ref,
-  ) {
+  ConsumerState<BasketScreen> createState() => _BasketScreenState();
+}
+
+class _BasketScreenState extends ConsumerState<BasketScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final basket = ref.watch(basketProvider);
+
+    if (basket.isEmpty) {
+      return const DefaultLayout(
+        title: '장바구니',
+        child: Center(
+          child: Text(
+            '장바구니가 비어있습니다 ㅠㅠ',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      );
+    }
+
+    final productsTotal = basket.fold<int>(
+      0,
+      (p, n) => p + (n.product.price * n.count),
+    );
+    final deliveryFee = (basket.first.product.restaurant).deliveryFee;
+
     return DefaultLayout(
       title: '장바구니',
-      child: Column(
-        children: [],
+      child: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.separated(
+                  separatorBuilder: (_, index) {
+                    return const Divider(
+                      height: 32.0,
+                    );
+                  },
+                  itemBuilder: (_, index) {
+                    final model = basket[index];
+                    return ProductCard.fromProductModel(
+                      model: model.product,
+                      onAdd: () {
+                        ref.read(basketProvider.notifier).addToBasket(
+                              product: model.product,
+                            );
+                      },
+                      onSubtract: () {
+                        ref.read(basketProvider.notifier).removeFromBasket(
+                              product: model.product,
+                            );
+                      },
+                    );
+                  },
+                  itemCount: basket.length,
+                ),
+              ),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        '장바구니 금액',
+                        style: TextStyle(color: BODY_TEXT_COLOR),
+                      ),
+                      Text(
+                        // fold : 배열의 조건을 누적시켜 반환한다.
+                        '₩$productsTotal',
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        '배달비',
+                        style: TextStyle(color: BODY_TEXT_COLOR),
+                      ),
+                      if (basket.isNotEmpty)
+                        Text(
+                          '₩$deliveryFee',
+                        )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        '총액',
+                        style: TextStyle(color: BODY_TEXT_COLOR),
+                      ),
+                      Text(
+                        (deliveryFee + productsTotal).toString(),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        final resp =
+                            await ref.read(orderProvider.notifier).postOrder();
+
+                        if (resp) {
+                          setState(() {
+                            context.goNamed(OrderDoneScreen.routeName);
+                          });
+                        } else {
+                          setState(() {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('결제 실패!'),
+                              ),
+                            );
+                          });
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: PRIMARY_COLOR,
+                      ),
+                      child: const Text(
+                        '결제하기',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/user/provider/auth_provider.dart
+++ lib/user/provider/auth_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_delivery_app/common/view/root_tab.dart';
 import 'package:flutter_delivery_app/common/view/splash_screen.dart';
+import 'package:flutter_delivery_app/order/view/order_done_screen.dart';
 import 'package:flutter_delivery_app/restaurant/view/basket_screen.dart';
 import 'package:flutter_delivery_app/restaurant/view/restaurant_detail.screen.dart';
 import 'package:flutter_delivery_app/user/model/user_model.dart';
@@ -52,6 +53,11 @@ class AuthProvider extends ChangeNotifier {
           path: '/basket',
           name: BasketScreen.routeName,
           builder: (_, state) => BasketScreen(),
+        ),
+        GoRoute(
+          path: '/order_done',
+          name: OrderDoneScreen.routeName,
+          builder: (_, state) => OrderDoneScreen(),
         ),
         GoRoute(
           path: '/splash',
